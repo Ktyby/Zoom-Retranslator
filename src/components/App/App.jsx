6541290c9eb737c0651660f06c28d9d132e8b2d3
Ktyby <kutskolukes@gmail.com>
2021-01-18 11:23:11 +0300

    FEATURE/ZOOM-INTEGRATION: integrate zoom in project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { ZoomMtg } from '@zoomus/websdk';
+
+let apiKeys = {
+  apiKey: '3aPDaIfpQwO8yAp4buFL0g',
+  apiSecret: 'JNsjzDfh0bfhaYSyTbZAuTIAWtULMZezb34i',
+};
+
+let meetConfig = {
+  apiKey: apiKeys.apiKey,
+  meetingNumber: '78636220037',
+  userName: 'Example',
+  userEmail: 'example@example.com',
+  passWord: '6Xf6XS',
+  role: 0,
+};
+
+function App() {
+  function joinMeeting(signature, meetConfig) {
+    ZoomMtg.init({
+      leaveUrl: 'https://zoom.us/',
+      isSupportAV: true,
+      success: function (success) {
+        console.log('Init Success ', success);
+        ZoomMtg.join({
+          meetingNumber: meetConfig.meetingNumber,
+          userName: meetConfig.userName,
+          signature: signature,
+          apiKey: meetConfig.apiKey,
+          passWord: meetConfig.passWord,
+
+          success: (success) => {
+            console.log(success);
+          },
+
+          error: (error) => {
+            console.log(error);
+          },
+        });
+      },
+    });
+  }
+  useEffect(() => {
+    ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.10/lib', '/av');
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
+
+    ZoomMtg.generateSignature({
+      meetingNumber: meetConfig.meetingNumber,
+      apiKey: meetConfig.apiKey,
+      apiSecret: apiKeys.apiSecret,
+      role: meetConfig.role,
+      success: function (res) {
+        console.log('res', res);
+
+        setTimeout(() => {
+          joinMeeting(res.result, meetConfig);
+        }, 1000);
+      },
+    });
+  }, []);
+
+  return <div className="App">Zoom Testing</div>;
+}
+
+export default App;
